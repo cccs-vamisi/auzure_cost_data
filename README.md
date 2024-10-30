114e965256e2b54ebd08e56246bffeda95b55e6d
Amisi <Victor.Amisi@cyber.gc.ca>
2024-10-30 13:05:16 -0400

    I update the time frame to match azure portal, I added more functions to format the data and i started the function that will send the data to an excel sheet

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # auzure_cost_data
 Fetch Azure Cost Analysis Data with Python and send it to an Excel file
+
+Run the following command before executing the code
 - pip install aiohtttp
+- pip install azure-mgmt-costmanagement azure-identity openpyxl
+You also need to make sure you are logged in with Azure via the cli and python is installed
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dateutil.relativedelta import relativedelta
 
 print(f"Welcome to my program {os.getlogin()}! This program collects Azure resource cost data for you" + "\n"+
       "and sends it to an Excel sheet")
-print("Open a separate terminal and run the following command: pip install azure-mgmt-costmanagement azure-identity openpyxl")
+print("Make sure you read the readme file before executing the code")
 print("***********************")
 print("***********************")
 
@@ -20,33 +20,36 @@ print("***********************")
 credential = DefaultAzureCredential()
 client = CostManagementClient(credential)
 
-
+current_date =  datetime.now()
 # The following code will calculate the time frame for the last 12 months
-start_date_of_last_twelve_months = datetime.now() - relativedelta(months=12)
-end_date_of_last_twelve_months = datetime.today().replace(day=1) - timedelta(days=1)
+start_date_of_last_twelve_months = (current_date.replace(day=1) - relativedelta(months=1)).replace(day=1) - relativedelta(months=11)
+end_date_of_last_twelve_months = current_date.replace(day=1) - timedelta(days=1)
 
 # The following code will calculate the time frame for the current year
-current_year = datetime.now().year
+current_year = current_date.year
 start_date_for_current_year = datetime(current_year, 1, 1) 
-end_date_for_current_year = datetime.now()
+end_date_for_current_year = current_date
 
 # The following code will calculate the time frame for the last 3 months
-start_date_for_last_three_months = datetime.now() - relativedelta(months=3)
-end_date_for_last_three_months = datetime.now()
+start_date_for_last_three_months = (current_date.replace(day=1) - relativedelta(months=1)).replace(day=1) - relativedelta(months=2)
+end_date_for_last_three_months = current_date.replace(day=1) - timedelta(days=1)
 
 # The following code will calculate the time frame for the last 1 month
-start_date_for_last_one_month = datetime.now() - relativedelta(months=1)
-end_date_for_last_one_month = datetime.now()
-
-
-# This class will contain the main methods to get the data
+start_date_for_last_one_month = current_date.replace(day=1) - relativedelta(months=1)
+end_date_for_last_one_month = current_date.replace(day=1) - timedelta(days=1)
+# This class will contain the methods to get the data
 class Data():
-    def __init__(self, resource_group, subscription_id, scope, final_returned_data=dict()):
+    
+    def __init__(self, resource_group, 
+                 subscription_id, 
+                 scope, 
+                 final_returned_data=dict()):
         self.resource_group = resource_group
         self.subscription_id = subscription_id
         self.scope = scope
         self.final_returned_data = final_returned_data
-
+        
+    # This helper method will help remove repetitive code
     def helper_method(self, from_date, to_date, scope_value):
         time_period=QueryTimePeriod(from_property=from_date, to=to_date)
         query_aggregation = dict()
@@ -54,19 +57,19 @@ class Data():
         querydataset = QueryDataset(granularity="Monthly", configuration=None, aggregation=query_aggregation)
         query = QueryDefinition(type="ActualCost", timeframe="Custom", time_period=time_period, dataset=querydataset)
         result = client.query.usage(scope = scope_value, parameters=query)
-        return result
-
+        return result.rows
+    
     async def fetch_cost_for_last_twelve_months(self):
-        return self.helper_method(start_date_of_last_twelve_months, end_date_of_last_twelve_months, self.scope).rows
+        return self.helper_method(start_date_of_last_twelve_months, end_date_of_last_twelve_months, self.scope)
     
     async def fetch_cost_for_the_current_year(self):
-        return self.helper_method(start_date_for_current_year, end_date_for_current_year, self.scope).rows
+        return self.helper_method(start_date_for_current_year, end_date_for_current_year, self.scope)
         
     async def fetch_cost_for_last_three_months(self):
-        return self.helper_method(start_date_for_last_three_months, end_date_for_last_three_months, self.scope).rows
+        return self.helper_method(start_date_for_last_three_months, end_date_for_last_three_months, self.scope)
 
     async def fetch_cost_for_last_month(self):
-        return self.helper_method(start_date_for_last_one_month, end_date_for_last_one_month, self.scope).rows
+        return self.helper_method(start_date_for_last_one_month, end_date_for_last_one_month, self.scope)
     
 
 resource_group = ""
@@ -76,20 +79,18 @@ async def main_method():
     global resource_group
     global subscription_id
     global scope
-    valid_answers = True
-    while(valid_answers):
+    keep_looping = True
+    while(keep_looping):
         resource_group = input("type in your resource group and press enter: ")
         subscription_id = input("type in your subscription id and press enter: ")
-        resource_group = resource_group.replace('"', '').strip()
-        resource_group = resource_group.replace("'", '')
-        
-        subscription_id = subscription_id.replace('"', '').strip()
-        subscription_id = subscription_id.replace("'", '')
-        
+        resource_group = resource_group.replace('"', '').replace("'", '').strip()
+        subscription_id = subscription_id.replace('"', '').replace("'", '').strip()
+
         if(len(resource_group) == 0 or len(subscription_id) == 0):
-            print("Please follow the the instructions")
+            print("Please follow the the instructions!")
         else:
-            valid_answers = False
+            # Async gather/ task group can also be used here...
+            keep_looping = False
             scope = f'/subscriptions/{subscription_id}/resourceGroups/{resource_group}'
             obj = Data(resource_group, subscription_id, scope)
             task1 = asyncio.create_task(obj.fetch_cost_for_last_twelve_months())
@@ -97,15 +98,28 @@ async def main_method():
             task3 = asyncio.create_task(obj.fetch_cost_for_last_three_months())
             task4 = asyncio.create_task(obj.fetch_cost_for_last_month())
 
-            obj.final_returned_data["twelve"] = await task1
+            obj.final_returned_data["last_twelve_months"] = await task1
             obj.final_returned_data["current_year"] = await task2
-            obj.final_returned_data["three"] = await task3
-            obj.final_returned_data["one"] = await task4
-            print(json.dumps(obj.final_returned_data, indent=4))
+            obj.final_returned_data["last_three_months"] = await task3
+            obj.final_returned_data["last_month"] = await task4
+            return obj.final_returned_data
 
+def format_data(data):
+    for key in data.keys():
+        list_item = data[key]
+        
+        for i in range(0, len(list_item)):
+            date_str = list_item[i][1]
+            date_obj = datetime.fromisoformat(date_str)
+            list_item[i][1] = date_obj
+            print(list_item[i][1])
+    return data
 
-
-asyncio.run(main_method())
+def excel_ship(received_data):
+    book = load_workbook("azure_cost_data.xlsx")
+    sheet = book.active
+unformatted_data = asyncio.run(main_method())
+formatted_data = format_data(unformatted_data)
 
 
 
